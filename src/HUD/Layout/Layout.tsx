46ef9ab8a9753dcc5e69219fa2fe0e56453e1702
Alex <alexzlkl@yandex.ru>
2022-01-15 20:17:50 +0300

    Update sidebox

--- src/HUD/Layout/Layout.tsx
+++ src/HUD/Layout/Layout.tsx
@@ -7,28 +7,28 @@ import { CSGO, Team } from "csgogsi-socket";
 import { Match } from "../../api/interfaces";
 import RadarMaps from "./../Radar/RadarMaps";
 import Trivia from "../Trivia/Trivia";
-import SideBox from '../SideBoxes/SideBox';
+import SideBox from "../SideBoxes/SideBox";
 import { GSI, actions } from "./../../App";
-import MoneyBox from '../SideBoxes/Money';
-import UtilityLevel from '../SideBoxes/UtilityLevel';
+import MoneyBox from "../SideBoxes/Money";
+import UtilityLevel from "../SideBoxes/UtilityLevel";
 import Killfeed from "../Killfeed/Killfeed";
 import MapSeries from "../MapSeries/MapSeries";
 import Overview from "../Overview/Overview";
 import Tournament from "../Tournament/Tournament";
 import Pause from "../PauseTimeout/Pause";
 import Timeout from "../PauseTimeout/Timeout";
-import MVP from './../MVP/mvp';
+import MVP from "./../MVP/mvp";
 import Firepower from "../Firepower/firepower";
 
 interface Props {
-  game: CSGO,
-  match: Match | null
+  game: CSGO;
+  match: Match | null;
 }
 
 interface State {
-  winner: Team | null,
-  showWin: boolean,
-  forceHide: boolean
+  winner: Team | null;
+  showWin: boolean;
+  forceHide: boolean;
 }
 
 export default class Layout extends React.Component<Props, State> {
@@ -37,16 +37,16 @@ export default class Layout extends React.Component<Props, State> {
     this.state = {
       winner: null,
       showWin: false,
-      forceHide: false
-    }
+      forceHide: false,
+    };
   }
 
   componentDidMount() {
-    GSI.on('roundEnd', score => {
+    GSI.on("roundEnd", (score) => {
       this.setState({ winner: score.winner, showWin: true }, () => {
         setTimeout(() => {
-          this.setState({ showWin: false })
-        }, 4000)
+          this.setState({ showWin: false });
+        }, 4000);
       });
     });
     actions.on("boxesState", (state: string) => {
@@ -62,81 +62,156 @@ export default class Layout extends React.Component<Props, State> {
     const { game, match } = this.props;
     const { map } = game;
     if (!match) return null;
-    const mapName = map.name.substring(map.name.lastIndexOf('/') + 1);
-    const veto = match.vetos.find(veto => veto.mapName === mapName);
+    const mapName = map.name.substring(map.name.lastIndexOf("/") + 1);
+    const veto = match.vetos.find((veto) => veto.mapName === mapName);
     if (!veto) return null;
     return veto;
-  }
+  };
 
   render() {
     const { game, match } = this.props;
-    const left = game.map.team_ct.orientation === "left" ? game.map.team_ct : game.map.team_t;
-    const right = game.map.team_ct.orientation === "left" ? game.map.team_t : game.map.team_ct;
-    const leftPlayers = game.players.filter(player => player.team.side === left.side);
-    const rightPlayers = game.players.filter(player => player.team.side === right.side);
-    const leftPlayersAlive = leftPlayers.filter(player => player.state.health > 0);
-    const rightPlayersAlive = rightPlayers.filter(player => player.state.health > 0);
-    const EquipmentFull = rightPlayersAlive.map(player => player.state.equip_value).reduce((pre, now) => pre + now, 0) + leftPlayersAlive.map(player => player.state.equip_value).reduce((pre, now) => pre + now, 0)
-    const isFreezetime = (game.round && game.round.phase === "freezetime") || game.phase_countdowns.phase === "freezetime";
-    const isUtilityLevel = (game.round && game.round.phase === "freezetime") || game.phase_countdowns.phase === "bomb";
-    const isFirepower = (game.round && game.round.phase === "freezetime") || game.phase_countdowns.phase === "live" || game.phase_countdowns.phase === "over" || game.phase_countdowns.phase === "warmup";
+    const left =
+      game.map.team_ct.orientation === "left"
+        ? game.map.team_ct
+        : game.map.team_t;
+    const right =
+      game.map.team_ct.orientation === "left"
+        ? game.map.team_t
+        : game.map.team_ct;
+    const leftPlayers = game.players.filter(
+      (player) => player.team.side === left.side
+    );
+    const rightPlayers = game.players.filter(
+      (player) => player.team.side === right.side
+    );
+    const leftPlayersAlive = leftPlayers.filter(
+      (player) => player.state.health > 0
+    );
+    const rightPlayersAlive = rightPlayers.filter(
+      (player) => player.state.health > 0
+    );
+    const EquipmentFull =
+      rightPlayersAlive
+        .map((player) => player.state.equip_value)
+        .reduce((pre, now) => pre + now, 0) +
+      leftPlayersAlive
+        .map((player) => player.state.equip_value)
+        .reduce((pre, now) => pre + now, 0);
+    const isFreezetime =
+      (game.round && game.round.phase === "freezetime") ||
+      game.phase_countdowns.phase === "freezetime";
+    const isUtilityLevel =
+      (game.round && game.round.phase === "freezetime") ||
+      game.phase_countdowns.phase === "bomb";
+    const isFirepower =
+      (game.round && game.round.phase === "freezetime") ||
+      game.phase_countdowns.phase === "live" ||
+      game.phase_countdowns.phase === "over" ||
+      game.phase_countdowns.phase === "warmup";
     const { forceHide } = this.state;
 
     return (
       <div className="layout">
-        <div className={`players_alive`}> 
+        <div className={`players_alive`}>
           <div className="title_container">Players alive</div>
           <div className="counter_container">
-            <div className={`team_counter ${left.side}`}>{leftPlayers.filter(player => player.state.health > 0).length}</div>
+            <div className={`team_counter ${left.side}`}>
+              {leftPlayers.filter((player) => player.state.health > 0).length}
+            </div>
             <div className={`vs_counter`}>VS</div>
-            <div className={`team_counter ${right.side}`}>{rightPlayers.filter(player => player.state.health > 0).length}</div>
+            <div className={`team_counter ${right.side}`}>
+              {rightPlayers.filter((player) => player.state.health > 0).length}
+            </div>
           </div>
         </div>
         <Killfeed />
         <Overview match={match} map={game.map} players={game.players || []} />
         <RadarMaps match={match} map={game.map} game={game} />
-        <MatchBar map={game.map} phase={game.phase_countdowns} bomb={game.bomb} match={match} />
+        <MatchBar
+          map={game.map}
+          phase={game.phase_countdowns}
+          bomb={game.bomb}
+          match={match}
+        />
 
-		<MVP isFreezetime={isFreezetime} veto={this.getVeto()} round={game.map.round+1}/>
-        <Pause  phase={game.phase_countdowns}/>
+        <MVP
+          isFreezetime={isFreezetime}
+          veto={this.getVeto()}
+          round={game.map.round + 1}
+        />
+        <Pause phase={game.phase_countdowns} />
         <Timeout map={game.map} phase={game.phase_countdowns} />
         <SeriesBox map={game.map} phase={game.phase_countdowns} match={match} />
 
         <Tournament />
 
-        <Observed player={game.player} veto={this.getVeto()} round={game.map.round+1}/>
+        <Observed
+          player={game.player}
+          veto={this.getVeto()}
+          round={game.map.round + 1}
+        />
 
-        <TeamBox team={left} players={leftPlayers} side="left" current={game.player} isFreezetime={isFreezetime} />
-        <TeamBox team={right} players={rightPlayers} side="right" current={game.player} isFreezetime={isFreezetime} />
+        <TeamBox
+          team={left}
+          players={leftPlayers}
+          side="left"
+          current={game.player}
+          isFreezetime={isFreezetime}
+        />
+        <TeamBox
+          team={right}
+          players={rightPlayers}
+          side="right"
+          current={game.player}
+          isFreezetime={isFreezetime}
+        />
 
         <Trivia />
 
-        <MapSeries teams={[left, right]} match={match} isFreezetime={isFreezetime} map={game.map} />
-        <div className={"boxes left"}>
-        <SideBox side="left" hide={forceHide} />  
+        <MapSeries
+          teams={[left, right]}
+          match={match}
+          isFreezetime={isFreezetime}
+          map={game.map}
+        />
+        <SideBox side="left">
           <MoneyBox
             team={left.side}
             side="left"
             loss={Math.min(left.consecutive_round_losses * 500 + 1400, 3400)}
-            equipment={leftPlayers.map(player => player.state.equip_value).reduce((pre, now) => pre + now, 0)}
-            money={leftPlayers.map(player => player.state.money).reduce((pre, now) => pre + now, 0)}
+            equipment={leftPlayers
+              .map((player) => player.state.equip_value)
+              .reduce((pre, now) => pre + now, 0)}
+            money={leftPlayers
+              .map((player) => player.state.money)
+              .reduce((pre, now) => pre + now, 0)}
             show={isFreezetime}
           />
-          <UtilityLevel side={left.side} players={game.players} show={isUtilityLevel} />
-        </div>
-        <div className={"boxes right"}>
-        <SideBox side="right" hide={forceHide} />  
-          <MoneyBox 
+          <UtilityLevel
+            side={left.side}
+            players={game.players}
+            show={isUtilityLevel}
+          />
+        </SideBox>
+        <SideBox side="right">
+          <MoneyBox
             team={right.side}
             side="right"
-            loss={Math.min(right.consecutive_round_losses * 500 + 1400, 3400)} 
-            equipment={rightPlayers.map(player => player.state.equip_value).reduce((pre, now) => pre + now, 0)}
-            money={rightPlayers.map(player => player.state.money).reduce((pre, now) => pre + now, 0)}
+            loss={Math.min(right.consecutive_round_losses * 500 + 1400, 3400)}
+            equipment={rightPlayers
+              .map((player) => player.state.equip_value)
+              .reduce((pre, now) => pre + now, 0)}
+            money={rightPlayers
+              .map((player) => player.state.money)
+              .reduce((pre, now) => pre + now, 0)}
             show={isFreezetime}
-          />  
-          <UtilityLevel side={right.side} players={game.players} show={isUtilityLevel} />
-          
-        </div>
+          />
+          <UtilityLevel
+            side={right.side}
+            players={game.players}
+            show={isUtilityLevel}
+          />
+        </SideBox>
       </div>
     );
   }
--- src/HUD/SideBoxes/SideBox.tsx
+++ src/HUD/SideBoxes/SideBox.tsx
@@ -1,42 +1,55 @@
 import React from 'react';
 import './sideboxes.scss'
-import {configs, hudIdentity} from './../../App';
-import { apiUrl } from '../../api/api';
+// import {configs, hudIdentity} from './../../App';
+// import { apiUrl } from '../../api/api';
 
-export default class SideBox extends React.Component<{ side: 'left' | 'right', hide: boolean}, { title: string, subtitle: string, image?: string }> {
-	constructor(props: any) {
-		super(props);
-		this.state = {
-            title:'Title',
-            subtitle:'Content'
-		}
-	}
+// export default class SideBox extends React.Component<{ side: 'left' | 'right', hide: boolean}, { title: string, subtitle: string, image?: string }> {
+// 	constructor(props: any) {
+// 		super(props);
+// 		this.state = {
+//             title:'Title',
+//             subtitle:'Content'
+// 		}
+// 	}
 
-	componentDidMount() {
-        configs.onChange((data:any) => {
-            if(!data) return;
-            const display = data.display_settings;
-            if(!display) return;
-            if(display[`${this.props.side}_title`]){
-                this.setState({title:display[`${this.props.side}_title`]})
-            }
-            if(display[`${this.props.side}_subtitle`]){
-                this.setState({subtitle:display[`${this.props.side}_subtitle`]})
-            }
-            if(display[`${this.props.side}_image`]){
-                this.setState({image:display[`${this.props.side}_image`]})
-            }
-        });
-	}
+// 	componentDidMount() {
+//         configs.onChange((data:any) => {
+//             if(!data) return;
+//             const display = data.display_settings;
+//             if(!display) return;
+//             if(display[`${this.props.side}_title`]){
+//                 this.setState({title:display[`${this.props.side}_title`]})
+//             }
+//             if(display[`${this.props.side}_subtitle`]){
+//                 this.setState({subtitle:display[`${this.props.side}_subtitle`]})
+//             }
+//             if(display[`${this.props.side}_image`]){
+//                 this.setState({image:display[`${this.props.side}_image`]})
+//             }
+//         });
+// 	}
 	
-	render() {
-        if(!this.state.title) return '';
-		return (
-			<div className={`sidebox ${this.props.side} ${this.props.hide ? 'hide':''}`}>
-                <div className="image_container">
-                    {this.state.image ? <img src={`data:image/jpeg;base64,${this.state.image}`} id={`image_left`} alt={'Left'}/>:''}
-                </div>
-                </div>
-		);
-    }
+// 	render() {
+//         if(!this.state.title) return '';
+// 		return (
+// 			<div className={`sidebox ${this.props.side} ${this.props.hide ? 'hide':''}`}>
+//                 <div className="image_container">
+//                     {this.state.image ? <img src={`data:image/jpeg;base64,${this.state.image}`} id={`image_left`} alt={'Left'}/>:''}
+//                 </div>
+//                 </div>
+// 		);
+//     }
+// }
+
+type SideBoxProps= {
+    side:'left' | 'right';
+    children: React.ReactNode;
+}
+
+export default function SideBox(props: SideBoxProps) {
+    return (
+        <div className={`boxes ${props.side}`}>
+            {props.children}
+        </div>
+    )
 }
