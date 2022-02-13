73751340c035db7d7a159293ee41d74ec75d69c6
Alex <alexzlkl@yandex.ru>
2022-02-13 14:01:34 +0300

    Update MVP

--- src/HUD/MVP/mvp.tsx
+++ src/HUD/MVP/mvp.tsx
@@ -1,97 +1,100 @@
 import React from "react";
 import { Player } from "csgogsi-socket";
 import { GSI } from "../../App";
-import Avatar from './../Players/Avatar';
+import Avatar from "./../Players/Avatar";
 import "./mvp.scss";
 import { Veto } from "../../api/interfaces";
 import * as I from "csgogsi-socket";
-import { Match } from "../../api/interfaces";
 import TeamLogo from "./../MatchBar/TeamLogo";
+import classNames from "classnames";
 interface State {
-    player: Player | null;
-    show: boolean;
-    round: number;
+  player: Player | null;
+  show: boolean;
+  round: number;
 }
 
 interface IProps {
-    map: I.Map;
+  map: I.Map;
 }
-export default class MVP extends React.Component<{isFreezetime:boolean, veto: Veto | null, round: number,}, State, IProps> {
+export default class MVP extends React.Component<
+  { isFreezetime: boolean; veto: Veto | null; round: number },
+  State,
+  IProps
+> {
+  constructor(p: { isFreezetime: boolean; veto: Veto | null; round: number }) {
+    super(p);
+    this.state = {
+      player: null,
+      show: false,
+      round: 0,
+    };
+  }
 
-    constructor(p: {isFreezetime:boolean, veto: Veto | null, round: number}){
-        super(p);
-        this.state = {
-            player: null,
-            show: false,
-            round: 0,
-        }
-    }
-	getAdr = () => {
-        const { veto } = this.props;
-        const { player } = this.state;
-		if (!player || !veto || !veto.rounds) return null;
-		const damageInRounds = veto.rounds.map(round => round.players[player.steamid]).filter(data => !!data).map(roundData => roundData.damage);
-        const adr = damageInRounds.reduce((a, b) => a + b, 0) / (this.props.round - 1);
-		return adr.toFixed(0);
-	}
-    componentDidMount(){
-        GSI.on("data", data => {
-            if(data.phase_countdowns.phase === "freezetime"){
-                this.setState({show: true});
-            }
-            else {
-                this.setState({show: false});
-            }
-        })
-        GSI.on("mvp", player => {
-            this.setState({
-                player,
-                show: true,
-            });
-        })
-    }
+  getAdr = () => {
+    const { veto, player } = this.props;
+    if (!player || !veto || !veto.rounds) return null;
+    const damageInRounds = veto.rounds
+      .map((round) => round.players[player.steamid])
+      .filter((data) => !!data)
+      .map((roundData) => roundData.damage);
+    return damageInRounds.reduce((a, b) => a + b, 0) / (this.props.round - 1);
+  };
+  componentDidMount() {
+    GSI.on("data", (data) => {
+      if (data.phase_countdowns.phase === "freezetime") {
+        this.setState({ show: true });
+      } else {
+        this.setState({ show: false });
+      }
+    });
+    GSI.on("mvp", (player) => {
+      this.setState({
+        player,
+        show: true,
+      });
+    });
+  }
   render() {
-    const { player, show, } = this.state;
-    const map = this.props;
-    const round = map.round - 1;
-    if(!player) return null;
+    const { player, show } = this.state;
+    if (!player) return null;
     return (
-      <div className={`mvp ${show ? 'show':'hide'}`}>
-          <div className={`data-container ${player.team.side} ${show ? 'show':'hide'}`}>
-              <div className={`damageinround ${player.team.side}`}>
-                  <div className="damageinround">DAMAGE ROUND</div>
-                  <div className={`data-damageinround ${player.team.side}`}>{player.state.round_totaldmg}
-              </div>
-              </div>
-              <div className={`data-entry ${player.team.side}`}>
-                  <div className="data-kills">KILLS</div>
-                  <div className={`dkills ${player.team.side}`}>{player.state.round_kills}
-              </div>
-              
-              <div className={`data-entry ${player.team.side}`}>
-                  <div className="data-round-damage">ADR</div>
-                  <div className={`round-damage ${player.team.side}`}>{this.getAdr()}
-              </div>
-              </div>
-              <div className={`avatar-container  ${player.team.side} ${show ? 'show':'hide'}`}>
-              <div className="avatar">
-                <Avatar steamid={player.steamid} width={170} height={170}/>
-              </div>
-              <div className={`nickname ${player.team.side}`}>
-                  {player.name}
-                
-              </div>
-              <div className="realname">{player.realName}</div>
+      <div
+        className={classNames(
+          "mvp",
+          { "mvp--show": show },
+          { "mvp--hide": !show },
+          { [`mvp--${player.team.side}`]: player }
+        )}
+      >
+        <div className="mvp__row">
+          <div className="kills">
+            <div className="kills__title">KILLS</div>
+            <div className="kills__count">{player.state.round_kills}</div>
           </div>
-          <div className="logo_MVP">
-          <TeamLogo team={player.team} height={35} width={35} />
-              </div>
-              </div>
-              <div className={`MVP_text ${player.team.side}`}>MVP</div>
+          <div className="mvp_title">MVP</div>
+        </div>
+        <div className="mvp__row">
+          <div className="damage-in-round">
+            <div className="damage-in-round__title">DAMAGE ROUND</div>
+            <div className="damage-in-round__count">
+              {player.state.round_totaldmg}
+            </div>
           </div>
-          <div className="MVP_fon"></div>
+          <div className="ADR_title">ADR</div>
+        </div>
+        <div className="mvp_avatar">
+          <Avatar steamid={player.steamid} />
+        </div>
+        <div className="mvp_player_info">
+          <div className="mvp_player_info__info">
+            <div className="mvp_player_info__nick-name">{player.name}</div>
+            <div className="mvp_player_info__name">{player.realName}</div>
+          </div>
+          <div className="mvp_player_info__team-logo">
+            <TeamLogo team={player.team} />
+          </div>
+        </div>
       </div>
-
     );
   }
 }
--- src/HUD/Players/Avatar.tsx
+++ src/HUD/Players/Avatar.tsx
@@ -1,47 +1,52 @@
-import React from 'react';
+import React from "react";
 // import { isDev, port } from '../../api/api';
-import { actions } from '../../App';
-import { avatars } from './../../api/avatars';
-import { Avatarka, Skull } from './../../assets/Icons';
+import { actions } from "../../App";
+import { avatars } from "./../../api/avatars";
+import { Avatarka, Skull } from "./../../assets/Icons";
 
 interface IProps {
-  steamid: string,
-  slot?: number,
-  height?: string | number | undefined,
-  width?: string | number | undefined,
-  showSkull?: boolean,
-  showCam?: boolean
+  steamid: string;
+  slot?: number;
+  height?: string | number | undefined;
+  width?: string | number | undefined;
+  showSkull?: boolean;
+  showCam?: boolean;
 }
 interface IState {
-  enableCams: boolean
+  enableCams: boolean;
 }
 export default class Avatar extends React.Component<IProps, IState> {
   state = {
-    enableCams: !!this.props.showCam
-  }
-  componentDidMount(){
+    enableCams: !!this.props.showCam,
+  };
+  componentDidMount() {
     actions.on("toggleCams", () => {
-      this.setState({enableCams: !this.state.enableCams})
-  });
+      this.setState({ enableCams: !this.state.enableCams });
+    });
   }
-  render(){
+  render() {
     // const { enableCams } = this.state;
     //const url = avatars.filter(avatar => avatar.steamid === this.props.steamid)[0];
     const avatarData = avatars[this.props.steamid];
-    if(!Avatarka && !avatarData.url.length){
-        return '';
+    if (!Avatarka && !avatarData.url.length) {
+      return "";
     }
     // const slot = this.props.slot === 0 ? 10 : this.props.slot || 1;
     // const leftPosition = - 150*((slot-1)%5);
     // const topPosition = slot > 5 ? -150 : 0;
     return (
       <div className={`player__avatar`}>
-          {
-            this.props.showSkull ? <Skull height={this.props.height} width={this.props.width} /> : <img src={ avatarData.url || Avatarka} height={this.props.height} width={this.props.width} alt={'Avatar'} />
-          }
-          
+        {this.props.showSkull ? (
+          <Skull height={this.props.height} width={this.props.width} />
+        ) : (
+          <img
+            src={avatarData.url || Avatarka}
+            height={this.props.height ? this.props.height : "100%"}
+            width={this.props.width ? this.props.width : "100%"}
+            alt={"Avatar"}
+          />
+        )}
       </div>
     );
   }
-
 }
